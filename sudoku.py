758138bc0dbaf90620236d8c09110b5eec09e85b
Lucernasancta <tommaso.pattaro@hotmail.com>
2025-05-18 17:08:35 +0200

    Added 9x1 check for the sudoku

--- sudoku.py
+++ sudoku.py
@@ -5,26 +5,19 @@ import numpy as np
 class Sudoku:
     def __init__(self):
 
-        self.create_sudoku()
+        # Get 9x9 grid of 0's to represent empty cells
+        self._original = np.zeros(shape=(9,9), dtype=int)
+        self._attempt = np.zeros(shape=(9,9), dtype=int)
     
     def create_sudoku(self, values: list = []) -> None:
-        '''
-        Create the numpy values from a list of existing values,
-        create a new one if no value is provided
-        '''
-
-        if not values:
-            # Get 9x9 grid of 0's to represent empty cells
-            self.values = np.zeros(shape=(9,9), dtype=int)
-            return
         
-        elif type(values[0][0]) is int:
-            self.values = np.array(values)
+        if type(values[0][0]) is int:
+            self._original = np.array(values)
 
         elif type(values[0][0]) is str:
-            self.values = np.array(values) # Create numpy array
-            self.values[self.values == ' '] = '0' # Change all the spaces with 0's
-            self.values = self.values.astype(np.int8) # Transform array from str to int
+            self._original = np.array(values) # Create numpy array
+            self._original[self._original == ' '] = '0' # Change all the spaces with 0's
+            self._original = self._original.astype(int) # Transform array from str to int
 
     def import_from_file(self, file_path: str) -> None:
 
@@ -34,15 +27,37 @@ class Sudoku:
 
             self.create_sudoku(list(spamreader))
     
-    def print_current(self) -> None:
-        print(self.values)
+    @property
+    def current(self) -> np.array:
+        return self._original
     
-    def check(self) -> bool:
-        '''Chack if the current configuration is valid, returns False if not'''
-        pass
+    def check9x1(self, value: np.ndarray) -> bool:
+        '''
+        Check if an array like object is valid, returns False if not.
+        To be valid:
+         - all values need to be unique (exept for 0's)
+         - all elemetns need to be between 1 and 9
+         - the sum must be 45 or less (9+8+7+6+5+4+3+2+1)
+        '''
+
+        value = value[value != 0] # Remove all the 0's
+
+        # Check if all values are unique
+        if len(value) != len(np.unique(value)):
+            return False
+
+        # check if array values lie in the range [1, 9]
+        if not ((value >= 1) & (value <= 9)).all():
+            return False
+        
+        # Check if the sum is less than 45
+        if sum(value) > 45:
+            return False
+
+        # The array is valid
+        return True
 
 
 if __name__ == '__main__':
     sudoku = Sudoku()
     sudoku.import_from_file('example.csv')
-    sudoku.print_current()
