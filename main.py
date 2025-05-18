788420b9e4b27a15b60eff7b071eb91a5ea7db29
Lucernasancta <tommaso.pattaro@hotmail.com>
2025-05-18 18:05:58 +0200

    Adde main function to the main script

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 import sys
 from sudoku import Sudoku
 
-# Create arguments dictionary
-args_dict = {
-    '-i': False
-}
+def main() -> None:
+
+    # Create arguments dictionary
+    args_dict = {
+        '-i': False
+    }
 
 
-args = sys.argv[1:]
-# Read every argument
-while args:
+    args = sys.argv[1:]
+    # Read every argument
+    while args:
 
-    match args[0]:
+        match args[0]:
 
-        # Import argument
-        case '-i':
-            try:
-                args_dict['-i'] = args[1]
-                args.pop(1)
-            except IndexError:
-                print('The -i flag must be followed by a file path')
-            
-    args.pop(0)
-
-
-# Create the sudoku
-sudoku = Sudoku()
+            # Import argument
+            case '-i':
+                try:
+                    args_dict['-i'] = args[1]
+                    args.pop(1)
+                except IndexError:
+                    print('The -i flag must be followed by a file path')
+                
+        args.pop(0)
 
 
-# Chack for the arguments
-if args_dict['-i']:
-    sudoku.import_from_file(args_dict['-i'])
+    # Create the sudoku
+    sudoku = Sudoku()
 
 
-sudoku.print_current()
+    # Chack for the arguments
+    if args_dict['-i']:
+        sudoku.import_from_file(args_dict['-i'])
+
+
+    sudoku.print_current()
+
+if __name__ == '__main__':
+    main()
